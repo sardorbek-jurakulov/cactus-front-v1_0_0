15ee8b449c8cd64dd0a1b1685cfb994191906fc2
“sardorbek-jurakulov” <sardorbek.juraqulov@gmail.com>
2025-01-31 13:49:23 +0500

    WIP

---
+++ components/TodoForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form class="todo-form">
+    <div class="todo-form__insert-elements-wrapper">
+      <label class="todo-form__label" for="title">Title</label>
+      <input class="todo-form__input" type="text" name="title" id="title" required>
+    </div>
+
+    <div class="todo-form__insert-elements-wrapper">
+      <label class="todo-form__label" for="description">Description</label>
+      <textarea class="todo-form__input" type="text" name="description" id="description" required>
+      </textarea>
+    </div>
+
+    <button type="submit" class="btn-success">Add todo</button>
+  </form>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+  .todo-form {
+    background-color: #fff;
+    padding: 10px;
+    margin: 20px 0px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 5px black;
+  }
+
+  .todo-form__insert-elements-wrapper {
+    margin-bottom: 10px;
+  }
+
+  .todo-form__label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+  }
+</style>
--- pages/todoes/[id].vue
+++ pages/todoes/[id].vue
@@ -9,7 +9,7 @@
     <p>Created At: 12:00</p>
 
     <NuxtLink :to="`/todoes/edit/{$1}`" class="btn">Edit</NuxtLink>
-    <button>Mark As Completed</button>
+    <button class="btn-success">Mark As Completed</button>
   </div>
 </template>
 
@@ -30,5 +30,38 @@
 
   .btn:hover {
     background-color: #0056b3;
+    color: #fff;
+  }
+
+  .btn-success {
+    background-color: #28a745;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+    font-size: 16px;
+  }
+
+  .btn-success:hover {
+    background-color: #218838;
+  }
+
+  .btn-danger {
+    background-color: #dc3545;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+    font-size: 16px;
+  }
+
+  .btn-danger:hover {
+
   }
 </style>
