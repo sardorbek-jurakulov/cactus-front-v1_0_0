5aae3dc6a980a61cf5d963d08c0b39aaf4e376ac
“sardorbek-jurakulov” <sardorbek.juraqulov@gmail.com>
2025-02-04 11:57:16 +0500

    error page hosil qilindi

---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Error! {{ error }}</h2>
+    <button @click="handleError">Go to home page </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import type { NuxtError } from '#app';
+  const props = defineProps({
+    error: Object as () => NuxtError,
+  });
+
+  const handleError = () => {
+    return clearError({
+      redirect: '/'
+    });
+  }
+</script>
