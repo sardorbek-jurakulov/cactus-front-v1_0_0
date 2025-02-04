0bfa6aa857fd09d557948478cd3df59c36f03d32
“sardorbek-jurakulov” <sardorbek.juraqulov@gmail.com>
2025-02-04 14:18:17 +0500

    server api function is creating.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   app: {
-    pageTransition: {
-      name: 'page',
-      mode: 'out-in'
-    },
+    // pageTransition: {
+    //   name: 'page',
+    //   mode: 'out-in'
+    // },
     head: {
       title: 'Nuxt mastering',
       meta: [
---
+++ server/api/todoes.ts
@@ -0,0 +1,64 @@
+import { defineEventHandler, readBody } from "h3";
+
+const todoes = [
+  {
+    id: 1,
+    title: "Sample Todo",
+    description: "This is a sample todo",
+    isCompleted: false,
+    createdAt: new Date().toISOString()
+  }
+];
+
+export default defineEventHandler (async (event) => {
+  const { req, res } = event;
+
+  // Get all todoes
+  if (req.method === "GET") {
+    return { success: true, data: todoes, statusCode: 201 };
+  }
+
+  // Add new todo
+  if (req.method === "POST") {
+    const newTodo = await readBody(event);
+    todoes.push(newTodo);
+    return {
+      value: newTodo,
+      statusCode: 201,
+    }
+  }
+
+  // Update todo
+  if (req.method === 'PUT') {
+    const updatedTodo = await readBody(event);
+    const todoIndex = todoes.findIndex((todo) => todo.id === updatedTodo.id);
+
+    if (todoIndex !== -1) {
+      todoes[todoIndex] = updatedTodo;
+
+      return {
+        value: updatedTodo,
+        statusCode: 201,
+      };
+    }
+    return {
+      statusCode: 404,
+    }
+  }
+
+  // Delete todo
+  if (req.method === 'DELETE') {
+    const { id } = await readBody(event);
+    const todoIndex = todoes.findIndex((todo) => todo.id === id );
+
+    if (todoIndex !== -1) {
+      todoes.splice(todoIndex, 1);
+      return {
+        success: true,
+        statusCode: 204
+      };
+    }
+  }
+
+  return { error: 'Invalid method' };
+});
